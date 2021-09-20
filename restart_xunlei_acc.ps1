90aa930fa70a32345b87f208f24855e222911e3f
Vincent Qiu (HOHO``) <nov30th@gmail.com>
2021-09-21 01:43:52 +0800

    Create restart_xunlei_acc.ps1

---
+++ restart_xunlei_acc.ps1
@@ -0,0 +1,26 @@
+echo testing network speed
+
+Get-Date >> C:\temp\speed.txt
+C:/speedtest.exe -s 7509 -p no -f json > C:\temp\speed.json
+
+$json = Get-Content C:\temp\speed.json | ConvertFrom-Json
+
+echo $json.download.bandwidth
+$json >> C:\temp\speed.txt
+
+if ($json.download.bandwidth -ge 25000000){
+    echo "larger" >> C:\temp\speed.txt
+    exit
+}else{
+    echo "smaller" >> C:\temp\speed.txt
+    echo Stopping XlNetAcc.exe
+    taskkill /f /im XlNetAcc.exe
+    net stop XlNetAccSrv
+    echo cding...
+    C:
+    cd "C:\Program Files (x86)\Thunder Network\XlNetAcc\Program"
+    echo Starting XlNetAcc.exe
+    net start XlNetAccSrv
+    start XlNetAcc.exe
+    echo "restarted" >> C:\temp\speed.txt
+}
